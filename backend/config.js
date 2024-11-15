3048aa0f6c67bed3af6ac4733bbe9d57fcd421ba
shalwin04 <shalwinsanju.25cs@licet.ac.in>
2024-11-15 20:02:03 +0530

    update

---
+++ backend/config.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    throw new Error(`Could not connect to MongoDB: ${err}`);
+  });
